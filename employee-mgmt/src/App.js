59aa45842421e57abdf038f3878077ee5eed2bf6
Shreyitha <shreyithashetty@gmail.com>
2024-02-19 11:42:54 +0530

    new commit

--- employee-mgmt/src/App.js
+++ employee-mgmt/src/App.js
@@ -1,19 +1,41 @@
+import { useEffect } from 'react';
+import EmployeForm from './EmpForm';
 import Emps from './Emps';
+import NewEmploye from './NewEmp';
+// import {useState}from 'react';
+// import { EmpContext } from './emp-context';
+import EmpContextProvider from './emp-context';
+import {useSelector,useDispatch} from 'react-redux';
+import { sendEmpData } from './Store';
 
-function App() {
-  const emp=[
-    {id:'e1',name:'Shreyitha',doj:new Date(2022,8,12),exp:2},
-    {id:'e2',name:'Harshitha',doj:new Date(2020,5,2),exp:1},
-    {id:'e3',name:'Manisha',doj:new Date(2023,12,8),exp:4},
-    {id:'e4',name:'Sakshi', doj:new Date(2021,3,7),exp:3},
-]
 
+const App=() =>{
+  const items=useSelector(state=>state.items);
+  const dispatch=useDispatch();
+
+
+  useEffect(
+    ()=>{
+      dispatch(sendEmpData(items))
+
+      // fetch('https://empreact-default-rtdb.firebaseio.com/emp.json',{
+      //   method:'PUT',
+      //   body:JSON.stringify(items)
+      // })
+    },
+    [items,dispatch]
+  );
 
   return (
-    <div>
-    <h1> Let's get started</h1>
-    <Emps items={emp}/>
-    </div>
+    <EmpContextProvider>
+      <div>
+        <h1> Let's get started</h1>
+        <NewEmploye >
+          <EmployeForm />
+        </NewEmploye>
+        <Emps/>
+      </div>
+    </EmpContextProvider>
   );
 }
 
